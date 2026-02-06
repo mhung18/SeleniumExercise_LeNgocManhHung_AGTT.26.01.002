dc529bade3e8a0e23f86fe517847dfbfaaef5da7
Manh Hung <lengocmanhhung0108@gmail.com>
2026-02-06 10:30:50 +0700

    push Tc 7,8,9

---
+++ PageObjects/Guerrillamail/MainPage.java
@@ -0,0 +1,71 @@
+package Guerrillamail;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import Common.Utilities;
+import Constant.Constant;
+import Railway.RegisterPage;
+
+public class MainPage {
+	private final By _emailName = By.id("inbox-id");
+	private final By _emailNameTextboxBy = By.xpath("//span[@id=\"inbox-id\"]/input[@type=\"text\"]");
+	private final By _btnSetEmail = By.xpath("//span[@id=\"inbox-id\"]/button[text()=\"Set\"]");
+	private final By _emailConfirm = By.xpath("//tbody[@id=\"email_list\"]//td[contains(text(),\"Please confirm your account\")]");
+	private final By _linkConfirm = By.xpath("//div[contains(@class,'email_body')]//a");
+	private final By _emailResetPassword = By.xpath("//tbody[@id=\"email_list\"]//td[contains(text(),\"Please reset your password\")]");
+	
+	public WebElement getEmailName() {
+		return Constant.WEBDRIVER.findElement(_emailName);
+	}
+	
+	public WebElement getEmailNameTextbox() {
+		return Constant.WEBDRIVER.findElement(_emailNameTextboxBy);
+	}
+	
+	public WebElement getBtnSetEmail() {
+		return Constant.WEBDRIVER.findElement(_btnSetEmail);
+	}
+	
+	public WebElement getEmailConfirm() {
+		return Constant.WEBDRIVER.findElement(_emailConfirm);
+	}
+	
+	public WebElement getLinkConfirm() {
+		return Constant.WEBDRIVER.findElement(_linkConfirm);
+	}
+	
+	public WebElement getEmailResetPassword() {
+		return Constant.WEBDRIVER.findElement(_emailResetPassword);
+	}
+	
+	public MainPage open() {
+		Constant.WEBDRIVER.navigate().to(Constant.GURERRILLAMAIL_URL);
+		return this;
+	}
+	
+	
+	public MainPage setEmailName(String emailName) {
+		Utilities.waitForElementVisible(_emailName, 10);
+		this.getEmailName().click();
+		this.getEmailNameTextbox().sendKeys(emailName);
+		this.getBtnSetEmail().click();
+		return this;
+	}
+	
+	public void activeAccount() {
+		Utilities.waitForElementClickable(_emailConfirm,20);
+		this.getEmailConfirm().click();
+		Utilities.waitForElementClickable(_linkConfirm,20);
+		this.getLinkConfirm().click();
+		Utilities.switchToLatestTab();
+	}
+	
+	public MainPage resetPassword() {
+		Utilities.waitForElementClickable(_emailResetPassword, 20);
+		this.getEmailResetPassword().click();
+		Utilities.waitForElementClickable(_linkConfirm, 10);
+		this.getLinkConfirm().click();
+		return this;
+	}
+}
---
+++ TestCases/Railway/CreateAccount.java
@@ -0,0 +1,133 @@
+package Railway;
+
+import org.openqa.selenium.WindowType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Common.Utilities;
+import Constant.Constant;
+import Guerrillamail.MainPage;
+
+public class CreateAccount extends BaseTest{
+	UserInfo userInfo = new UserInfo(
+		Constant.USERNAME, 
+		Constant.PASSWORD, 
+		Constant.PASSPORTID);
+
+	@Test
+	public void TC07() {
+		String expectedMsg = "This email address is already in use.";
+
+		System.out.println("User can't create account with an already in-use email");
+		
+		System.out.println("1. Navigate to QA Railway Website");
+		HomePage homePage = new HomePage();
+		homePage.open();
+		
+		System.out.println("2. Click on \"Register\" tab");
+		RegisterPage registerPage = homePage.goToPage("Register", RegisterPage.class);
+		
+		System.out.println("3. Enter information of the created account in Pre-condition");
+		System.out.println("4. Click on \"Register\" button");
+		registerPage.register(userInfo);
+		
+		System.out.println("VP: Error message \"This email address is already in use.\" displays above the form.");
+		String actualMsg = registerPage.getLblRegisterErrorMsg().getText();
+		
+		Assert.assertEquals(actualMsg, expectedMsg, "Error message is not displayed as expected");		
+	}
+	
+	@Test
+	public void TC08() {
+		System.out.println("User can't create account while password and PID fields are empty");
+		
+		System.out.println("1. Navigate to QA Railway Website");
+		HomePage homePage = new HomePage();
+		homePage.open();
+		
+		System.out.println("2. Click on \"Register\" tab");
+		RegisterPage registerPage = homePage.goToPage("Register", RegisterPage.class);
+		
+		System.out.println("3. Enter valid email address and leave other fields empty");
+		System.out.println("4. Click on \"Register\" button");
+		String emailString = Utilities.generateRandomEmail();
+		registerPage.regiter(
+				emailString, 
+				Constant.BLANK_PASSWORD, 
+				Constant.BLANK_PASSWORD, 
+				Constant.BLANK_PASSPORTID);
+		
+		System.out.println("VP: Message \"There're errors in the form. Please correct the errors and try again.\" appears above the form.\n"
+				+ "VP: Next to password fields, error message \"Invalid password length\" displays\n"
+				+ "VP: Next to PID field, error message \"Invalid ID length\" displays");
+		String actualMsg = registerPage.getLblRegisterErrorMsg().getText();
+		String expectedMsg = "There're errors in the form. Please correct the errors and try again.";
+		Assert.assertEquals(actualMsg, expectedMsg, "Error message is not displayed as expected");
+		
+		String actualPasswordErrorMsg = registerPage.getLblPasswordErrorMsg().getText();
+		String expectedPasswordErrorMsg = "Invalid password length";
+		Assert.assertEquals(actualPasswordErrorMsg, expectedPasswordErrorMsg, "Error message for password is not displayed as expected");
+		
+		String actualPassportIdErrorMsg = registerPage.getLblPassportIdErrorMsg().getText();
+		String expectedPassportIdErrorMsg = "Invalid ID length";
+		Assert.assertEquals(actualPassportIdErrorMsg, expectedPassportIdErrorMsg, "Error message for PID is not displayed as expected");
+	}
+	
+	@Test
+	public void TC09() throws InterruptedException {
+		String expectedHref = "http://saferailway.somee.com/Account/Register.cshtml";
+		String expectedMsg = "Thank you for registering your account";
+		String expectedRegisterConfirmStringMsg = "Registration Confirmed! You can now log in to the site.";
+
+		System.out.println("User create and activate account");
+		
+		System.out.println("1. Navigate to QA Railway Website");
+		HomePage homePage = new HomePage();
+		homePage.open();
+		
+		System.out.println("VP: Home page is shown with guide containing href \"create an account\" to \"Register\" page");
+		Utilities.scrollToEndPage();
+		String actuallinkText = homePage.getLinkToCreateAccount().getText();
+		String expectedLinkText = "create an account";
+		Assert.assertEquals(actuallinkText, expectedLinkText, "Link text is not displayed as expected");
+		
+		String actualHref = homePage.getLinkToCreateAccount().getDomProperty("href");
+		Assert.assertEquals(actualHref, expectedHref, "The link does not redirect to Register Page");
+		
+		System.out.println("2. Click on \"Create an account\"");
+		homePage.getLinkToCreateAccount().click();
+		RegisterPage registerPage = new RegisterPage();
+		String actualCurrentPageString = registerPage.getSelectedTabName();
+		Assert.assertEquals(actualCurrentPageString, "Register", "The link does not redirect to Register Page");
+		
+		System.out.println("3. Enter valid information into all fields");
+		System.out.println("4. Click on \"Register\" button");
+		String randomEmail = Utilities.generateRandomEmail();
+		registerPage.regiter(
+				randomEmail, 
+				Constant.PASSWORD, 
+				Constant.PASSWORD, 
+				Constant.PASSPORTID
+		);
+		
+		System.out.println("VP: \"Thank you for registering your account\" is shown");
+		String actualMsg = registerPage.getLblRegisterSuccessfully().getText();
+		Assert.assertEquals(actualMsg, expectedMsg, "The registered successful message is not displayed as expected");
+		
+		System.out.println("5. Get email information (webmail address, mailbox and password) and navigate to that webmail");
+		System.out.println("6. Login to the mailbox");
+		Constant.WEBDRIVER.switchTo().newWindow(WindowType.TAB).get(Constant.GURERRILLAMAIL_URL);
+		MainPage mainPageMailWeb = new MainPage();
+		mainPageMailWeb.setEmailName(Utilities.getEmailPartName(randomEmail));
+		
+		System.out.println("7. Open email with subject containing \"Please confirm your account\"  and the email of the new account at step 3");		
+		System.out.println("8. Click on the activate link");
+		mainPageMailWeb.activeAccount();
+		Utilities.switchToLatestTab();
+
+		System.out.println("VP: Redirect to Railways page and message \"Registration Confirmed! You can now log in to the site\" is shown");
+		RegisterPage registerConfirmPage = new RegisterPage();
+		String actualRegisterConfirmMsg = registerConfirmPage.getRegisterConfirmMsg();
+		Assert.assertEquals(actualRegisterConfirmMsg, expectedRegisterConfirmStringMsg, "Register Confirmation is not displayed as expected");
+	}
+}
